9ee300804508d44a1162400c01336eec31ee0a23
vinmarvin <162027699+szahraak@users.noreply.github.com>
2026-05-01 23:44:44 +0700

    Simpan perubahan sementara di main.dart

--- sintesa_virtual_lab/lib/main.dart
+++ sintesa_virtual_lab/lib/main.dart
@@ -13,7 +13,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const VirtualLabApp());
 }
 
 class VirtualLabApp extends StatelessWidget {
